bfa81f4e5c5bfe9b64cc0332fc3e422eb625dd98
p4nash <nash.riaz1995@gmail.com>
2021-06-29 18:11:51 +0500

    completed move over to node

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ var commandsManager = require('./commands.js');
 var commands = [];
 var ComfyJS = require("comfy.js");
 
-var auth_token = '';
-
 var app = express();
+
+app
+.use(express.static(__dirname + '/public'))
+.use(cors())
+.use(cookieParser())
+.use(bodyParser.json())
+.use(bodyParser.urlencoded({  extended:true }));
 
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if(user.toLowerCase() == process.env.TWITCH_USERNAME) return;
@@ -74,8 +79,171 @@ function RemoveACommand(com){
   }
 }
 
+var auth_token, client_token = '';
 app.listen(process.env.PORT || 8888, ()=>{
   console.log("Server started on port 8888");
+});
 
-  ComfyJS.Init(process.env.TWITCH_TARGET_CHANNEL, process.env.TWITCH_AUTH_CLIENT);
+app.get('/', function(req, res){
+  res.redirect('/index.html');
+});
+
+app.get('/login', function(req, res){
+  console.log("Authorizing user");
+  var options = 'https://id.twitch.tv/oauth2/authorize?'+
+     querystring.stringify({
+       client_id: process.env.CLIENT_ID,
+       redirect_uri: 'http://localhost:8888/feed.html',
+       response_type: 'token',
+       scope: 'channel:manage:redemptions channel:read:redemptions user:read:email chat:edit chat:read',
+       force_verify: true
+     });
+
+     res.redirect(options);
+});
+
+app.get('/feed', function(req, res){
+  client_token = req.query.access_token;
+  console.log("Logging in user using token "+client_token);
+  ComfyJS.Init(process.env.TWITCH_TARGET_CHANNEL, client_token);
+
+
+  res.setHeader('Content-Type', 'application/json');
+
+  ComfyJS.onChat = ( user, command, message, flags, extra ) => {
+    console.log( user + " send a message.");
+    res.write(JSON.stringify({
+      type: "chat",
+      user: user,
+      command: command,
+      message: message,
+      flags: flags,
+      extra: extra
+    }));
+  }
+
+  // ComfyJS.onReward = ( user, reward, cost, message, extra ) => {
+  //   console.log( user + " redeemed " + reward + " for " + cost );
+  //   res.send({
+  //     type: "reward",
+  //     user: user,
+  //     reward: reward,
+  //     cost: cost,
+  //     message: message,
+  //     extra: extra
+  //   });
+  // }
+  //
+  // ComfyJS.onHosted=(user, viewers, autohost, extra) => {
+  //   console.log( user + " hosted with " + viewers + " viewers.");
+  //   res.send({
+  //     type: "host",
+  //     user: user,
+  //     viewers: viewers,
+  //     autohost: autohost,
+  //     extra: extra
+  //   });
+  // }
+  //
+  // ComfyJS.onRaid=(user, viewers, extra) => {
+  //   console.log( user + " raided with " + viewers + " viewers.");
+  //   res.send({
+  //     type: "raid",
+  //     user: user,
+  //     viewers: viewers,
+  //     extra: extra
+  //   });
+  // }
+  //
+  // ComfyJS.onCheer=(user, message, bits, flags, extra) => {
+  //   console.log( user + " cheered for " + bits + " bits.");
+  //   res.send({
+  //     type: "cheer",
+  //     user: user,
+  //     message: message,
+  //     bits: bits,
+  //     flags: flags,
+  //     extra:extra
+  //   });
+  // }
+  //
+  // ComfyJS.onSub=(user, message, subTierInfo, extra) => {
+  //   console.log( user + " subbed with " + subTierInfo.planName + " tier.");
+  //   res.send({
+  //     type: "sub",
+  //     user: user,
+  //     message: message,
+  //     subTierInfo: subTierInfo,
+  //     extra:extra
+  //   });
+  // }
+  //
+  // ComfyJS.onResub=(user, message, streamMonths, cumulativeMonths, subTierInfo, extra) => {
+  //   console.log( user + " resubbed for " + streamMonths + " months.");
+  //   res.send({
+  //     type: "resub",
+  //     user: user,
+  //     message: message,
+  //     streamMonths: streamMonths,
+  //     cumulativeMonths: cumulativeMonths,
+  //     subTierInfo: subTierInfo,
+  //     extra:extra
+  //   });
+  // }
+  //
+  // ComfyJS.onSubGift=( gifterUser, streakMonths, recipientUser, senderCount, subTierInfo, extra ) => {
+  //   console.log( gifterUser + " gifted sub to " + recipientUser + " months.");
+  //   res.send({
+  //     type: "subgift",
+  //     user: user,
+  //     message: message,
+  //     streamMonths: streamMonths,
+  //     cumulativeMonths: cumulativeMonths,
+  //     subTierInfo: subTierInfo,
+  //     extra:extra
+  //   });
+  // }
+  //
+  // ComfyJS.onGiftSubContinue=(user, sender, extra) => {
+  //   console.log( user + " is continuing sub gifted by " + sender + ".");
+  //   res.send({
+  //     type: "giftsubcontinue",
+  //     user: user,
+  //     sender: sender,
+  //     extra:extra
+  //   });
+  // }
+  //
+  // ComfyJS.onSubMysteryGift=( gifterUser, numbOfSubs, senderCount, subTierInfo, extra ) => {
+  //   console.log( gifterUser + " was gifted a sub by anonymous.");
+  //   res.send({
+  //     type: "submysterygift",
+  //     gifterUser: gifterUser,
+  //     numbOfSubs: numbOfSubs,
+  //     senderCount: senderCount,
+  //     subTierInfo: subTierInfo,
+  //     extra:extra
+  //   });
+  // }
+
+  ComfyJS.onJoin=(user, self, extra) => {
+    console.log( user + " joined chat.");
+    res.write(JSON.stringify({
+      type: "join",
+      user: user,
+      self: self,
+      extra:extra
+    }));
+  }
+
+  ComfyJS.onPart=(user, self, extra) => {
+    console.log( user + " left chat.");
+    res.write(JSON.stringify({
+      type: "part",
+      user: user,
+      self: self,
+      extra:extra
+    }));
+  }
+
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,3 @@
-
 var usersInChat = [];
 var channelNameString = "";
 var chatterCounterOn = false;
@@ -7,148 +6,94 @@ var scrollEvents = scrollMessages = true;
 var isAutoScrollEvent = false;
 
 $(function(){
-  channelNameString=localStorage.getItem("twitch");
 
-  chatterCounterOn = localStorage.getItem("chatterCounterOn");
+  initialize();
 
-  if(chatterCounterOn == null) {
-    chatterCounterOn = true;
-    localStorage.setItem("chatterCounterOn", true);
-    $("#chatterCounterOn").prop("checked", true);
-  }else{
-    $("#chatterCounterOn").prop("checked", chatterCounterOn);
-    if(chatterCounterOn) $('#viewerCount').show();
-    else $('#viewerCount').hide();
-  }
+  var params = getHashParams();
 
-  chatterEventOn = localStorage.getItem("chatterEventOn");
+  var access_token = params.access_token;
+  console.log(access_token);
 
-  if(chatterEventOn == null) {
-    chatterEventOn = true;
-    localStorage.setItem("chatterEventOn", true);
-    $("#chatterEventOn").prop("checked", true);
-  }else{
-    $("#chatterEventOn").prop("checked", chatterEventOn);
-  }
+  $.ajax({
+          url: 'http://localhost:8888/feed?access_token='+access_token,
+          success: function(response) {
+            console.log(response);
+            switch(response.type){
+              case "chat":
+              AddMessage(response.user, response.command, response.extra);
+              break;
 
-  if(channelNameString == null)
-    window.location.href = "index.html";
+              case "join":
+              if(!chatterEventOn) return;
 
-  ComfyJS.onChat = ( user, command, message, flags, extra ) => {
-    AddMessage(user, command, extra);
-  }
+              AddEvent('https://img.icons8.com/material/24/42ff87/chat--v1.png',
+              "<span class='userEvent'>"+response.user+'</span> just joined.');
 
-  ComfyJS.onJoin=(user, self, extra) => {
-      console.log(user+' joined chat');
-      if(!chatterEventOn) return;
+              if(!usersInChat.find(findUsername, response.user)){
+                usersInChat.push(response.user);
+                $('#viewerCount').html('<img src="https://img.icons8.com/ios-glyphs/30/ffffff/visible.png"/>'+usersInChat.length);
+              }
+              break;
 
-      AddEvent('https://img.icons8.com/material/24/42ff87/chat--v1.png',
-      "<span class='userEvent'>"+user+'</span> just joined.');
+              case "part":
+              if(!chatterEventOn) return;
 
-      if(!usersInChat.find(findUsername, user)){
-        usersInChat.push(user);
-        $('#viewerCount').html('<img src="https://img.icons8.com/ios-glyphs/30/ffffff/visible.png"/>'+usersInChat.length);
-      }
-  }
+              AddEvent('https://img.icons8.com/material/24/ff6ba1/chat--v1.png',
+              "<span class='userEvent'>"+response.user+'</span> just left.');
 
-  ComfyJS.onPart=(user, self, extra) => {
-      console.log(user+' left chat');
-      if(!chatterEventOn) return;
+              if(usersInChat.find(findUsername, response.user))
+              {
+                var index = usersInChat.indexOf(response.user);
+                if(index > -1){
+                  usersInChat.splice(index, 1);
+                  $('#viewerCount').html('<img src="https://img.icons8.com/ios-glyphs/30/ffffff/visible.png"/>'+usersInChat.length);
+                }
+              }
+              break;
 
-      AddEvent('https://img.icons8.com/material/24/ff6ba1/chat--v1.png',
-      "<span class='userEvent'>"+user+'</span> just left.');
+              case "reward":
+              AddEvent('https://img.icons8.com/material/24/ff6ba1/sport-badge.png',
+              "<span class='userEvent'>"+response.user+'</span> claimed the reward '+response.reward+
+              "<span class='messageEvent'>"+response.message+"</span>");
+              break;
 
-      if(usersInChat.find(findUsername, user))
-      {
-        var index = usersInChat.indexOf(user);
-        if(index > -1){
-          usersInChat.splice(index, 1);
-          $('#viewerCount').html('<img src="https://img.icons8.com/ios-glyphs/30/ffffff/visible.png"/>'+usersInChat.length);
-        }
-      }
-  }
+              case "host":
+              break;
 
-  ComfyJS.onHosted=(user, viewers, autohost, extra) => {
-      console.log(user+' hosted');
+              case "raid":
+              break;
 
-      AddEvent('https://img.icons8.com/android/24/ff6ba1/retro-tv.png',
-      "<span class='userEvent'>"+user+'</span> hosted with '+viewers+' viewers.');
-  }
+              case "cheer":
+              break;
 
-  ComfyJS.onRaid=(user, viewers, extra) => {
-      console.log(user+' raided');
+              case "sub":
+              break;
 
-      AddEvent('https://img.icons8.com/ios-glyphs/30/ff6ba1/baby-footprints-path.png',
-      "<span class='userEvent'>"+user+'</span> raided with '+viewers+' viewers.');
-  }
+              case "resub":
+              break;
 
-  ComfyJS.onCheer=(user, message, bits, flags, extra) => {
-      console.log(user+' cheered');
+              case "subgift":
+              break;
 
-      var content = "<span class='userEvent'>"+user+'</span> cheered '+bits+' bits.';
+              case "giftsubcontinue":
+              break;
 
-      if(message != null)
-        content = content + ' They said: <span class="italic">"'+message+'"</span>.';
-
-      AddEvent('https://img.icons8.com/ios-filled/50/42ff87/diamond--v1.png',
-      content);
-  }
-
-  ComfyJS.onSub=(user, message, subTierInfo, extra) => {
-      console.log(user+' subbed. ', subTierInfo.planName);
-
-      var content = "<span class='userEvent'>"+user+'</span> subbed with '+subTierInfo.planName+' sub.';
-      if(message != null)
-        content = content + ' They said: <span class="italic">"'+message+'"</span>.'
-      AddEvent('https://img.icons8.com/material-sharp/24/42ff87/star.png',content);
-  }
-
-  ComfyJS.onResub=(user, message, streamMonths, cumulativeMonths, subTierInfo, extra) => {
-      console.log(user+' subbed. ', subTierInfo.planName, message);
-
-      var content =  "<span class='userEvent'>"+user+'</span> subbed with '+subTierInfo.planName+' sub. They\'ve been subbed for  <span class="bold">'+cumulativeMonths+' months</span>.';
-
-       if(message != null)
-        content = content + ' They said: <span class="italic">"' + message + '"</span>.'
-
-      AddEvent('https://img.icons8.com/material-sharp/24/ffd000/star.png', content);
-  }
-
-  ComfyJS.onSubGift=( gifterUser, streakMonths, recipientUser, senderCount, subTierInfo, extra ) => {
-      console.log(user+' subbed. ', subTierInfo.planName);
-
-      AddEvent('https://img.icons8.com/metro/26/42ff87/gift.png',
-      "<span class='userEvent'>"+gifterUser+'</span> gifted a '+subTierInfo.planName+' sub to '+recipientUser+'. They\'ve gifted '+senderCount+' subs so far!');
-  }
-
-  ComfyJS.onGiftSubContinue=(user, sender, extra) => {
-      console.log(user+' resubbed');
-
-      AddEvent('https://img.icons8.com/metro/26/ffd000/gift.png',
-      "<span class='userEvent'>"+sender+'</span> continued '+user+'\'s sub for them!');
-  }
-
-  ComfyJS.onSubMysteryGift=( gifterUser, numbOfSubs, senderCount, subTierInfo, extra ) => {
-      console.log(user+' subbed. ', subTierInfo.planName);
-
-      AddEvent('https://img.icons8.com/pastel-glyph/64/42ff87/christmas-gift--v2.png',
-      "<span class='userEvent'>"+gifterUser+'</span> gifted '+numbOfSubs+' '+subTierInfo.planName+' subs to the channel! They\'ve gifted '+senderCount+' to the channel.');
-  }
-
-  ComfyJS.Init( channelNameString );
-
-  $('#channelName').html(channelNameString);
-
-  console.log("Ready!");
-
-  $( ".buttons>img" ).click(function() {
-    $.ajax({
-        url: 'http://localhost:8888/handleUser'+userID,
-        success: function(response) {}
-      });
-  });
-
+              case "submysterygift":
+              break;
+            }
+          }
+        });
 });
+
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
 
 function AddEvent(image, content){
   var userElement = "<div class='event'><img src='"+image+"'/>"+
@@ -187,7 +132,6 @@ function AddMessage(user, message, extra){
   if(scrollMessages)
     $('#messagesList').animate({scrollTop: $('#messagesList')[0].scrollHeight}, 500);
 }
-
 
 function isAddressedToSomeoneElse(content){
   var ats = content.search('@');
@@ -237,6 +181,34 @@ function logout(){
 
 function settings(){
   $('#settingsPanel').fadeIn();
+}
+
+function initialize(){channelNameString=localStorage.getItem("twitch");
+
+  chatterCounterOn = localStorage.getItem("chatterCounterOn");
+
+  if(chatterCounterOn == null) {
+    chatterCounterOn = true;
+    localStorage.setItem("chatterCounterOn", true);
+    $("#chatterCounterOn").prop("checked", true);
+  }else{
+    $("#chatterCounterOn").prop("checked", chatterCounterOn);
+    if(chatterCounterOn) $('#viewerCount').show();
+    else $('#viewerCount').hide();
+  }
+
+  chatterEventOn = localStorage.getItem("chatterEventOn");
+
+  if(chatterEventOn == null) {
+    chatterEventOn = true;
+    localStorage.setItem("chatterEventOn", true);
+    $("#chatterEventOn").prop("checked", true);
+  }else{
+    $("#chatterEventOn").prop("checked", chatterEventOn);
+  }
+  //
+  // if(channelNameString == null)
+  //   window.location.href = "index.html";
 }
 
 $('.close').click(function(){
